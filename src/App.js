258215155455d828e870896378e98fc3a938c960
fearm3 <ibraaa1628@gmail.com>
2022-01-04 15:02:26 +0300

    add commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import { useFetch } from "./useFetch";
 import Follower from "./Follower";
 function App() {
   const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page]);
+  }, [loading]);
   return (
     <main>
       <div className="section-title">
@@ -11,10 +18,21 @@ function App() {
       </div>
       <section className="followers">
         <div className="container">
-          {data.map((follower) => {
+          {followers.map((follower) => {
             return <Follower key={follower.id} {...follower} />;
           })}
         </div>
+        {!loading && (
+          <div className="btn-container">
+            {data.map((item, index) => {
+              return (
+                <button key={index} className="page-btn">
+                  {index + 1}
+                </button>
+              );
+            })}
+          </div>
+        )}
       </section>
     </main>
   );
