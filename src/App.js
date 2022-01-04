40e32b1f196c5f7958cb2f24a60936530684b6f4
fearm3 <ibraaa1628@gmail.com>
2022-01-04 15:34:47 +0300

    add final

--- src/App.js
+++ src/App.js
@@ -9,7 +9,30 @@ function App() {
   useEffect(() => {
     if (loading) return;
     setFollowers(data[page]);
-  }, [loading]);
+  }, [loading, page]);
+
+  const handlePage = (index) => {
+    setPage(index);
+  };
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
   return (
     <main>
       <div className="section-title">
@@ -24,13 +47,24 @@ function App() {
         </div>
         {!loading && (
           <div className="btn-container">
+            <button className="prev-btn" onClick={prevPage}>
+              prev
+            </button>
             {data.map((item, index) => {
               return (
-                <button key={index} className="page-btn">
+                <button
+                  key={index}
+                  className={`page-btn ${index === page && "active-btn"}`}
+                  onClick={() => handlePage(index)}
+                >
                   {index + 1}
                 </button>
               );
             })}
+
+            <button className="next-btn" onClick={nextPage}>
+              next
+            </button>
           </div>
         )}
       </section>
